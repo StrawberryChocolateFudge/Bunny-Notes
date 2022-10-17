cde11c6cf5b3d450d338d61ef97cedfa7f0f27bf
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-10-18 00:49:20 +0100

    Added more card data

--- frontend/components/utils/cardPropsData.ts
+++ frontend/components/utils/cardPropsData.ts
@@ -1,9 +1,17 @@
 import { SelectableCardsProps } from "../SelectableCards";
 
-export function getCardPropsData(cardType: 'Gift Card' | "Cash Note"): Array<SelectableCardsProps> {
-    return [
-        { imageLink: "https://raw.githubusercontent.com/StrawberryChocolateFudge/Bunny-Notes/master/frontend/logos/tetherLogo.svg", imageAlt: `10 USDT ${cardType}`, denomination: "10", currency: "USDT", cardType },
-        { imageLink: "https://raw.githubusercontent.com/StrawberryChocolateFudge/Bunny-Notes/master/frontend/logos/tetherLogo.svg", imageAlt: `100 USDT ${cardType}`, denomination: "100", currency: "USDT", cardType },
-        { imageLink: "https://raw.githubusercontent.com/StrawberryChocolateFudge/Bunny-Notes/master/frontend/logos/usddLogo.svg", imageAlt: `10 USDD ${cardType}`, denomination: "10", currency: "USDD", cardType },
-        { imageLink: "https://raw.githubusercontent.com/StrawberryChocolateFudge/Bunny-Notes/master/frontend/logos/usddLogo.svg", imageAlt: `100 USDD ${cardType}`, denomination: "100", currency: "USDD", cardType }]
+export function getCardPropsData(cardType: 'Gift Card' | "Cash Note" | "Payment Request"): Array<SelectableCardsProps> {
+
+    if (cardType === "Payment Request") {
+        return [
+            { imageLink: "https://raw.githubusercontent.com/StrawberryChocolateFudge/Bunny-Notes/master/frontend/logos/tetherLogo.svg", imageAlt: `100 USDT ${cardType}`, denomination: "100", currency: "USDT", cardType },
+            { imageLink: "https://raw.githubusercontent.com/StrawberryChocolateFudge/Bunny-Notes/master/frontend/logos/usddLogo.svg", imageAlt: `100 USDD ${cardType}`, denomination: "100", currency: "USDD", cardType },
+        ]
+    } else {
+        return [
+            { imageLink: "https://raw.githubusercontent.com/StrawberryChocolateFudge/Bunny-Notes/master/frontend/logos/tetherLogo.svg", imageAlt: `10 USDT ${cardType}`, denomination: "10", currency: "USDT", cardType },
+            { imageLink: "https://raw.githubusercontent.com/StrawberryChocolateFudge/Bunny-Notes/master/frontend/logos/tetherLogo.svg", imageAlt: `100 USDT ${cardType}`, denomination: "100", currency: "USDT", cardType },
+            { imageLink: "https://raw.githubusercontent.com/StrawberryChocolateFudge/Bunny-Notes/master/frontend/logos/usddLogo.svg", imageAlt: `10 USDD ${cardType}`, denomination: "10", currency: "USDD", cardType },
+            { imageLink: "https://raw.githubusercontent.com/StrawberryChocolateFudge/Bunny-Notes/master/frontend/logos/usddLogo.svg", imageAlt: `100 USDD ${cardType}`, denomination: "100", currency: "USDD", cardType }]
+    }
 }
