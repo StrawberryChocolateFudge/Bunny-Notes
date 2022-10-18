67db3d83e256d7ebe6dc5a23e008943412e628c5
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-10-19 00:22:57 +0100

    added handling to note select

--- frontend/components/PurchaseCashNote.tsx
+++ frontend/components/PurchaseCashNote.tsx
@@ -8,12 +8,13 @@ import TextField from '@mui/material/TextField';
 import Tooltip from '@mui/material/Tooltip';
 import WalletIcon from "@mui/icons-material/AccountBalanceWallet"
 import { getCardPropsData } from './utils/cardPropsData';
-import CardGrid from './CardGrid';
+import CardGrid, { CardType } from './CardGrid';
+import { BaseTronUser } from './Base';
+import { onBoardOrGetTronWeb, verifyAddress } from '../tron';
 
 
-interface PurchaseCashNoteProps {
-    myAddress: string,
-    setMyAddress: (newValue: string) => void
+interface PurchaseCashNoteProps extends BaseTronUser {
+
 }
 
 
@@ -21,6 +22,41 @@ export default function PurchaseCashNote(props: PurchaseCashNoteProps) {
 
     const addressSetter = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
         props.setMyAddress(event.target.value);
+    }
+
+    const handleSelectCashNote = async (denomination: string, currency: string, cardType: CardType) => {
+        let tronWeb = null;
+        if (props.tronWeb === null) {
+            tronWeb = await onBoardOrGetTronWeb(props.displayError);
+
+            if (tronWeb) {
+                props.setTronWeb(tronWeb);
+            }
+        }
+        const addressValid = verifyAddress(props.myAddress);
+        if (!addressValid) {
+            props.displayError("Invalid Address!");
+            return;
+        }
+        if (props.tronWeb === null && tronWeb === null) {
+            // if the address is valid but We can't get the tronWeb I also return
+            props.displayError("Unable to connect to wallet!")
+            return;
+        }
+        // Here if props.tronWeb was null, I use the new tronWeb otherwise use the props.
+        // This is relevant because of that first click that, it runs here before props.setTronWeb runs!
+        if (props.tronWeb === null) {
+            // use the tronWeb object
+        } else {
+            // use the props.tronWeb object
+        }
+        //TODO: Now I create a note with this denomination
+        // navigate to another page where the note can be downloaded
+
+        console.log(tronWeb);
+        console.log("DENOMINATIOn", denomination);
+        console.log("CURRENCY", currency);
+        console.log("Card Type", cardType);
     }
 
     return (
@@ -59,7 +95,7 @@ export default function PurchaseCashNote(props: PurchaseCashNoteProps) {
                     </Grid>
                 </Toolbar>
             </AppBar>
-            <CardGrid cardProps={getCardPropsData("Cash Note")} ></CardGrid>
+            <CardGrid handleSelect={handleSelectCashNote} cardProps={getCardPropsData("Cash Note")} ></CardGrid>
         </Paper>
     );
 }
--- frontend/components/PurchaseGiftCardTab.tsx
+++ frontend/components/PurchaseGiftCardTab.tsx
@@ -7,19 +7,70 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Tooltip from '@mui/material/Tooltip';
 import WalletIcon from "@mui/icons-material/AccountBalanceWallet"
-import CardGrid from './CardGrid';
+import CardGrid, { CardType } from './CardGrid';
 import { getCardPropsData } from './utils/cardPropsData';
+import { generateZKProof, createNote } from '../zkp/generateProof';
+import { onBoardOrGetTronWeb, verifyAddress } from '../tron';
+import { BaseTronUser } from './Base';
 
-interface GiftCardPageProps {
-    myAddress: string,
-    setMyAddress: (newValue: string) => void
+interface GiftCardPageProps extends BaseTronUser {
+
 }
 
 export default function PurchaseGiftCardTab(props: GiftCardPageProps) {
 
     const addressSetter = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-
         props.setMyAddress(event.target.value);
+    }
+
+    const importAddress = async () => {
+        if (props.tronWeb === null) {
+            const tronWeb = await onBoardOrGetTronWeb(props.displayError);
+
+            if (tronWeb) {
+                props.setMyAddress(tronWeb.defaultAddress.base58);
+                props.setTronWeb(tronWeb);
+            }
+
+        } else {
+            const defaultAddress = props.tronWeb.defaultAddress;
+            props.setMyAddress(defaultAddress.base58);
+        }
+    }
+
+    const handleSelectGiftCard = async (denomination: string, currency: string, cardType: CardType) => {
+        let tronWeb = null;
+        if (props.tronWeb === null) {
+            tronWeb = await onBoardOrGetTronWeb(props.displayError);
+            if (tronWeb) {
+                props.setTronWeb(tronWeb);
+            }
+        }
+        const addressValid = verifyAddress(props.myAddress);
+        if (!addressValid) {
+            props.displayError("Invalid Address!");
+            return;
+        }
+        if (props.tronWeb === null && tronWeb === null) {
+            // if the address is valid but We can't get the tronWeb I also return
+            props.displayError("Unable to connect to wallet!")
+            return;
+        }
+        // Here if props.tronWeb was null, I use the new tronWeb otherwise use the props.
+        // This is relevant because of that first click that, it runs here before props.setTronWeb runs!
+        if (props.tronWeb === null) {
+            // use the tronWeb object
+        } else {
+            // use the props.tronWeb object
+        }
+
+        //TODO: Now I create a note with this denomination
+        // navigate to another page where the note can be downloaded
+
+        console.log(tronWeb);
+        console.log("DENOMINATIOn", denomination);
+        console.log("CURRENCY", currency);
+        console.log("Card Type", cardType);
     }
 
     return (
@@ -50,7 +101,7 @@ export default function PurchaseGiftCardTab(props: GiftCardPageProps) {
                         </Grid>
                         <Grid item>
                             <Tooltip title="Import Address From Wallet Extension">
-                                <Button variant="contained" sx={{ mr: 1 }}>
+                                <Button onClick={importAddress} variant="contained" sx={{ mr: 1 }}>
                                     Import Address
                                 </Button>
                             </Tooltip>
@@ -58,9 +109,7 @@ export default function PurchaseGiftCardTab(props: GiftCardPageProps) {
                     </Grid>
                 </Toolbar>
             </AppBar>
-
-            <CardGrid cardProps={getCardPropsData("Gift Card")} ></CardGrid>
-
+            <CardGrid handleSelect={handleSelectGiftCard} cardProps={getCardPropsData("Gift Card")} ></CardGrid>
         </Paper>
     );
 }
