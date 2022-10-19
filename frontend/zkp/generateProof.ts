552dfb9b55fe768a7c6dfc9672ef5564761bdd86
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-10-19 22:08:58 +0100

    updated proof generation return types

--- frontend/zkp/generateProof.ts
+++ frontend/zkp/generateProof.ts
@@ -4,11 +4,22 @@ import { deposit, parseNote } from "../../lib/note";
 
 const urlBASE = "http://localhost:1234"
 
+const netId = 0x2b6653dc;
+
+
 export const snarkArtifacts = { wasmFilePath: urlBASE + "/withdraw.wasm", zkeyFilePath: urlBASE + "/withdraw_0001.zkey" };
 
-export async function createNote() {
-    const noteString = await deposit({ currency: "UDST", amount: 10, netId: 0x2b6653dc })
-    return await parseNote(noteString);
+export type NoteDetails = [
+    string, ParsedNote
+]
+
+export type ParsedNote = { currency: string, amount: string, netId: number, deposit: Deposit }
+
+export async function createNote(currency, amount): Promise<NoteDetails> {
+    const noteString = await deposit({ currency, amount, netId })
+    const parsedNote = await parseNote(noteString) as ParsedNote;
+
+    return [noteString, parsedNote];
 }
 
 export async function generateZKProof(deposit: Deposit, recepient: string, change: "0"): Promise<FullProof> {
