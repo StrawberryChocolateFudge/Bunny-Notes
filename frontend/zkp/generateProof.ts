9bd8ceea6d2bca40fd71b96a6fa718d686e6425e
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-10-24 00:33:34 +0100

    added packSolidityProof

--- frontend/zkp/generateProof.ts
+++ frontend/zkp/generateProof.ts
@@ -1,7 +1,7 @@
 import { Deposit, FullProof } from "../../lib/types";
 import { generateProof } from "../../lib/generateProof";
 import { deposit, parseNote } from "../../lib/note";
-
+import packToSolidityProof from "../../lib/packToSolidityProof";
 const urlBASE = "http://localhost:1234"
 
 const netId = 0x2b6653dc;
@@ -22,6 +22,10 @@ export async function createNote(currency, amount): Promise<NoteDetails> {
     return [noteString, parsedNote];
 }
 
-export async function generateZKProof(deposit: Deposit, recepient: string, change: "0"): Promise<FullProof> {
+export async function generateZKProof(deposit: Deposit, recepient: string, change: string): Promise<FullProof> {
     return await generateProof({ deposit, recepient, change, snarkArtifacts })
+}
+
+export function packSolidityProof(proof: any) {
+    return packToSolidityProof(proof);
 }
