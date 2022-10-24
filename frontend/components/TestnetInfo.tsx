d333b8f6549d3b61c6650dda049d6da95ab23403
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-10-25 00:32:13 +0100

    updated to use provider instead of tronWeb

--- frontend/components/TestnetInfo.tsx
+++ frontend/components/TestnetInfo.tsx
@@ -1,39 +1,57 @@
 import { Button, Paper, Typography } from "@mui/material";
 import React from "react";
-import { getContract, onBoardOrGetTronWeb, TESTNETMINTERC20, USDTMCONTRACTADDRESS } from "../tron";
-import { BaseTronUser } from "./Base";
+import { Base, Spacer } from "./Base";
 import { ethers } from "ethers";
-interface TestnetInfoProps extends BaseTronUser {
+import { getContract, onBoardOrGetProvider, requestAccounts, switchToDonauTestnet, TESTNETMINTERC20, USDTMCONTRACTADDRESS_DOTNAU, watchAsset } from "../web3/web3";
+interface TestnetInfoProps extends Base {
 }
 
 
 export function TestnetInfo(props: TestnetInfoProps) {
 
-    async function doMint(tronWeb: any) {
-        const contract = await getContract(tronWeb, USDTMCONTRACTADDRESS);
-        const address = tronWeb.defaultAddress.base58;
+    async function doMint(provider: any) {
+        const contract = await getContract(provider, USDTMCONTRACTADDRESS_DOTNAU, "/MOCKERC20.json");
+        const account = await requestAccounts(provider);
         const amount = ethers.utils.parseEther("1000");
-        await TESTNETMINTERC20(contract, address, amount);
+        await TESTNETMINTERC20(contract, account, amount).then(async () => {
+            await watchAsset(
+                {
+                    address: USDTMCONTRACTADDRESS_DOTNAU,
+                    symbol: "USDTM",
+                    decimals: 18
+                },
+                () => {
+                    props.displayError("Unable to add token")
+                })
+        });
     }
 
 
     const onMint = async () => {
-        if (props.tronWeb === null) {
-            const tronWeb = await onBoardOrGetTronWeb(props.displayError)
+        if (props.provider === null) {
+            const provider = await onBoardOrGetProvider(props.displayError)
 
-            if (tronWeb) {
-                props.setTronWeb(tronWeb);
-                await doMint(tronWeb);
+            if (provider) {
+                props.setProvider(provider);
+                await doMint(provider);
             }
         } else {
             // I call the mint function here too
-            await doMint(props.tronWeb);
+            await doMint(props.provider);
         }
+    }
+
+    const switchNetwork = async () => {
+        await switchToDonauTestnet()
+        // I set the provider null so we need to reconnect to avoid error after changing network!
+        props.setProvider(null);
     }
 
 
     return <Paper sx={{ maxWidth: 936, margin: '0 auto', padding: "5px" }}>
-        <Typography variant="subtitle1" component="div">We are on testnet! Mint some USDT Mock (USDTM) <Button onClick={onMint} variant="contained">Here</Button> to try out the app.</Typography>
+        <Typography variant="subtitle1" component="div"> <Button variant="contained" onClick={switchNetwork}>Switch</Button> to Donau Testnet!</Typography>
+        <Spacer></Spacer>
+        <Typography variant="subtitle1" component="div"><Button onClick={onMint} variant="contained">Mint</Button> some USDT Mock (USDTM)  to try out the app. </Typography>
     </Paper>
 }
 
