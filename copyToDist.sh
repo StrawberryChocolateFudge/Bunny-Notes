9e5bf9b89ab40650d05953339dd34b9b3ea09802
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-10-25 00:27:04 +0100

    added new copy script lines WIP

--- copyToDist.sh
+++ copyToDist.sh
@@ -5,4 +5,7 @@
 #cp Bunny.svg dist/Bunny.svg
 
 # copy the TronWeb dependency to use in the browser
-cp node_modules/tronweb/dist/TronWeb.js dist/tronweb.js
+#cp node_modules/tronweb/dist/TronWeb.js dist/tronweb.js
+
+cp artifacts/contracts/ERC20Notes.sol/ERC20Notes.json dist/ERC20Notes.json
+cp artifacts/contracts/Mock/MockERC20.sol/MOCKERC20.json dist/MOCKERC20.json
