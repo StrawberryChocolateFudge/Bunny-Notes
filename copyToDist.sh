02910ca8c95c097b1215f34935fd280a097e2faf
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-10-31 18:04:40 +0000

    moved the ABIs from gitignore

--- copyToDist.sh
+++ copyToDist.sh
@@ -2,8 +2,8 @@ cp circuits/withdraw_0001.zkey dist/withdraw_0001.zkey
 
 cp circuits/withdraw_js/withdraw.wasm dist/withdraw.wasm
 
-cp artifacts/contracts/ERC20Notes.sol/ERC20Notes.json dist/ERC20Notes.json
-cp artifacts/contracts/Mock/MockERC20.sol/MOCKERC20.json dist/MOCKERC20.json
+cp abis/ERC20Notes.json dist/ERC20Notes.json
+cp abis/MOCKERC20.json dist/MOCKERC20.json
 
 cp Bunny.svg dist/Bunny.svg
 cp testnet-banner.png dist/testnet-banner.png
