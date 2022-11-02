2da8db09ac6f75d608fbf4bd86e185a199de65b8
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-11-02 17:16:30 +0000

    moved images to a directory

--- frontend/components/HelpPage.tsx
+++ frontend/components/HelpPage.tsx
@@ -15,19 +15,17 @@ export default function HelpPage() {
             {Img("/Bunny-Note.jpg", "200px")}
             <hr />
             {Title("Help")}
-
             {Body(`Welcome to Bunny Notes. This is a step by step tutorial to show you how the application works!
                 Bunny Notes is a Gift Card and Cash Note protocol.It allows you to create cryptographic notes that you can spend in various ways after creating them.
                 You can do offline payments by handing other people the notes or you can pay safely with cash notes on the go without touching your crypto wallet while you can be sure the merchant has no access to your balance other than the note!`)}
             <hr />
             {STitle('We are on Testnet')}
-
             {Body("A Hot Wallet is needed to try out the DApp. You need to use Metamask or similar The Bunny Notes Smart contracts are deployed on Donau testnet and use the BitTorrent Chain!")}
-            {Img("/testnet-banner.png", "600px")}
+            {Img("/imgs/testnet-banner.png", "600px")}
             <hr />
             {STitle("Purchase Gift Cards or Cash Notes")}
             {Body("You can select a token from the list and purchase it. All you need is to provide your crypto address. Click \"Import Address\" to connect metamask and fill it out automatically. Select USDTM from the list to proceed with your purchase. Because we are on testnet, only that token works. Make sure you mint some first so you can make the deposit!")}
-            {Img("/depositPage.png", "600px")}
+            {Img("/imgs/depositPage.png", "600px")}
             {Body("Congratulations! After a successful deposit, your Note now has value! You can give it to somebody to cash it out or if you created a cash note, you can spend it!")}
             <hr />
             {STitle("Gift Cards VS Cash Notes")}
@@ -36,23 +34,23 @@ export default function HelpPage() {
             {STitle("The Note")}
             {Body("The Bunny Note is a cryptographic note, a secret number is generated with a nullifier (also a number) that is used to create a commitment that identifies the deposit. The PDF contains the secret in the QR code or on the bottom of the page. When withdrawing the value from the note, a zero-knowledge proof is generated to prove the owner owns the Note.")}
             <hr />
-            <iframe style={{ margin: "0 auto" }} width="500" height={'200'} src="/testNote.pdf"></iframe>
+            <iframe style={{ margin: "0 auto" }} width="500" height={'200'} src="/imgs/testNote.pdf"></iframe>
             <hr />
             {STitle("Verify a Note")}
             {Body("You should verify a note if you find one to check if it's valid. Use the QR code scanner or copy the text from the bottom of the pdf image to the input field and click verify!")}
-            {Img("/verifyPage.png", "600px")}
+            {Img("/imgs/verifyPage.png", "600px")}
             <hr />
             {STitle("Cash Out a Gift Card")}
             {Body("Gift cards can be converted back to tokens. When cashing out the user needs to scan the QR code or paste the note and they are ready to withdraw the tokens!")}
-            {Img("/cashOut.png", "600px")}
+            {Img("/imgs/cashOut.png", "600px")}
             <hr />
             {STitle("Request Payment")}
             {Body("Payment Requests are very versatile. Using this page will create a link to the application that you can share online or you can show it to your customer in person, and he can pay with the note instantly using the QR Code Scanner!")}
-            {Img("/paymentRequestPage.png", "600px")}
+            {Img("/imgs/paymentRequestPage.png", "600px")}
             <hr />
             {STitle("Payment Link Page")}
             {Body("The payment link page can be used to pay in person or online. In the current version one of the parties must use a crypto wallet however it can be either of the parties. A seller can just scan the QR code of the Note and the buyer can be sure the seller has no access to his wallet when making payments or in another scenario the seller can send the link to the buyer and the buyer will submit the transaction to the network. Either way, it works.")}
-            {Img("/payWithCashNote.png", "600px")}
+            {Img("/imgs/payWithCashNote.png", "600px")}
         </Box>
     </Box>
 }
