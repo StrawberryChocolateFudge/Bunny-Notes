dbeffdc48d1276ca5847ede53f739655c8993ac7
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-11-02 17:13:42 +0000

    moved images to a dir

--- copyToDist.sh
+++ copyToDist.sh
@@ -5,14 +5,18 @@ cp circuits/withdraw_js/withdraw.wasm dist/withdraw.wasm
 cp abis/ERC20Notes.json dist/ERC20Notes.json
 cp abis/MOCKERC20.json dist/MOCKERC20.json
 
-cp Bunny.svg dist/Bunny.svg
-cp testnet-banner.png dist/testnet-banner.png
 
-cp depositPage.png dist/depositPage.png
-cp testNote.pdf dist/testNote.pdf
+cp -R imgs dist/imgs
 
-cp verifyPage.png dist/verifyPage.png
-cp cashOut.png dist/cashOut.png
-cp paymentRequestPage.png dist/paymentRequestPage.png
-cp payWithCashNote.png dist/payWithCashNote.png
-cp Bunny-Note.jpg dist/Bunny-Note.jpg
+
+#cp Bunny.svg dist/Bunny.svg
+#cp testnet-banner.png dist/testnet-banner.png
+
+#cp depositPage.png dist/depositPage.png
+#cp testNote.pdf dist/testNote.pdf
+
+#cp verifyPage.png dist/verifyPage.png
+#cp cashOut.png dist/cashOut.png
+#cp paymentRequestPage.png dist/paymentRequestPage.png
+#cp payWithCashNote.png dist/payWithCashNote.png
+#cp Bunny-Note.jpg dist/Bunny-Note.jpg
