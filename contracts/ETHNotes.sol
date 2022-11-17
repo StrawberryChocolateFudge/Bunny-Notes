7a6a9b8d874ba1f494755a97403b5f10572b47d4
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-11-17 22:40:52 +0000

    created ETHNotes

---
+++ contracts/ETHNotes.sol
@@ -0,0 +1,49 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./BunnyNotes.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract ETHNotes is BunnyNotes {
+    constructor(
+        IVerifier _verifier,
+        uint256 _denomination,
+        uint8 _feeDivider,
+        address _relayer
+    ) BunnyNotes(_verifier, _denomination, _feeDivider, _relayer) {}
+
+    function _processDeposit(address depositFor) internal override {
+        if (msg.sender == relayer) {
+            require(msg.value == denomination, "Please send correct value");
+            // If the relayer makes this tx, there are no fees
+        } else {
+            require(msg.sender == depositFor,"You msut deposit for yourself");
+            require(
+                msg.value == denomination + fee,
+                "Please send correct value with fee"
+            );
+
+            // Forward the fee to the owner
+            Address.sendValue(_owner, fee);
+        }
+    }
+
+    function _processWithdrawGiftCard(address payable _recepient)
+        internal
+        override
+    {
+        Address.sendValue(_recepient, denomination);
+    }
+
+    function _processWithdrawCashNote(
+        address payable _recepient,
+        address payable _creator,
+        uint256 _price,
+        uint256 _change
+    ) internal override{
+        Address.sendValue(_recepient, _price);
+
+        if(_change > 0){
+            Address.sendValue(_creator, _change);
+        }
+    }
+}
