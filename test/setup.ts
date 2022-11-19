c38994915b2775ef963154ceb4e7d6239e9f94de
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-11-19 23:08:47 +0000

    updated setup to support ETHNotes

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,9 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
+import { ERC20Notes, ETHNotes, MOCKERC20 } from "../typechain";
 
-export async function setUp(): Promise<any> {
+export async function setUp(): Promise<{ owner: SignerWithAddress, alice: SignerWithAddress, bob: SignerWithAddress, USDTM: MOCKERC20, Verifier: Contract, ERC20Notes: ERC20Notes, relayer: SignerWithAddress, ETHNotes: ETHNotes, provider: any, attacker: SignerWithAddress }> {
 
     const DENOMINATION = ethers.utils.parseEther("10"); // 10 Dollars!
 
@@ -17,14 +20,18 @@ export async function setUp(): Promise<any> {
     await USDTM.mint(owner.address, ethers.utils.parseEther("100"));
 
 
-    const VerifierFactory = await ethers.getContractFactory("Verifier");
+    const VerifierFactory = await ethers.getContractFactory("contracts/WithdrawVerifier.sol:Verifier");
     const VerifierDeploy = await VerifierFactory.deploy();
     const Verifier = await VerifierDeploy.deployed();
 
     const ERC20NotesFactory = await ethers.getContractFactory("ERC20Notes");
+    const ETHNotesFactory = await ethers.getContractFactory("ETHNotes");
 
-    const ERC20Notes = await ERC20NotesFactory.deploy(VerifierDeploy.address, DENOMINATION, FEEDIVIDER, MockERC20.address, relayer.address);
 
+    const ERC20Notes: ERC20Notes = await ERC20NotesFactory.deploy(VerifierDeploy.address, DENOMINATION, FEEDIVIDER, MockERC20.address, relayer.address);
+    const ETHNotes: ETHNotes = await ETHNotesFactory.deploy(VerifierDeploy.address, DENOMINATION, FEEDIVIDER, relayer.address);
 
-    return { owner, alice, bob, attacker, USDTM, Verifier, ERC20Notes, relayer }
+    const provider = ethers.provider;
+
+    return { owner, alice, bob, attacker, USDTM, Verifier, ERC20Notes, relayer, ETHNotes, provider }
 }
