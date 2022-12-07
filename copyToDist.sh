52646e229904b4f73eaffc3a085ca4b19e87476c
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-12-07 12:33:46 +0000

    updated abis to copy

--- copyToDist.sh
+++ copyToDist.sh
@@ -5,11 +5,9 @@ cp circuits/withdraw_bunnyNote/withdraw_js/withdraw.wasm dist/withdraw.wasm
 cp circuits/wallet_owner/isOwner_0001.zkey dist/isOwner_0001.zkey
 cp circuits/wallet_owner/isOwner_js/isOwner.wasm dist/isOwner.wasm
 
-cp circuits/swap_verifier/swapVerfier_0001.zkey dist/swapVerfier_0001.zkey
-cp circuits/swap_verifier/swap_verifier_js/swapVerifier.wasm
-
 cp abis/ERC20Notes.json dist/ERC20Notes.json
+cp abis/ETHNotes.json dist/ETHNotes.json
+cp abis/BunnyWallet.json dist/BunnyWallet.json
 cp abis/MOCKERC20.json dist/MOCKERC20.json
-
 
 cp -R imgs dist/imgs
