9bddfd714b4c25ff1655faece1eac7d59e287464
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-12-16 19:33:16 +0100

    updated tabs!

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -21,42 +21,13 @@ interface HeaderProps {
 export default function Header(props: HeaderProps) {
     return (
         <React.Fragment>
-            <AppBar
-                component="div"
-                color="primary"
-                position="static"
-                elevation={0}
-                sx={{ zIndex: 0 }}
-            >
-                <Toolbar>
-                    <Grid container alignItems="center" spacing={1}>
-                        <Grid item xs>
-                            <Typography sx={{ marginLeft: "35px" }} color="inherit" variant="h5" component="h1"><Link sx={{ color: "white" }} href="/" underline="none">
-                                Bunny Notes
-                            </Link></Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography color="inherit" variant='subtitle1' component={"p"}>
-                                Gift Card and Cash Note Protocol
-                            </Typography>
-                        </Grid>
-                        <Grid item>
-                            <Tooltip title="Help">
-                                <IconButton color="inherit" href="/help">
-                                    <HelpIcon />
-                                </IconButton>
-                            </Tooltip>
-                        </Grid>
-                    </Grid>
-                </Toolbar>
-            </AppBar>
+
 
             {props.withTabs ? <AppBar component="div" position="static" elevation={0} sx={{ zIndex: 0 }}>
                 <Tabs value={props.selectedTab} onChange={props.onTabToggle} textColor="inherit" variant='scrollable' scrollButtons allowScrollButtonsMobile aria-label="Bunny Notes Options">
-                    <Tab label="Purchase Gift Cards" />
-                    <Tab label="Purchase Cash Notes" />
+                    <Tab label="Bunny Notes" />
                     <Tab label="Verify a Note" />
-                    <Tab label="Cash Out Gift Card" />
+                    <Tab label="Cash Out" />
                     <Tab label="Request Payment" />
                     <Tab label="Bunny Wallet"></Tab>
                 </Tabs>
