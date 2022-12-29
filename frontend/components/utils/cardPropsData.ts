b7361da4d0d59656b751e4087998ae90edc17779
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-12-29 02:06:31 +0100

    update: getCardPropsData uses the netid

--- frontend/components/utils/cardPropsData.ts
+++ frontend/components/utils/cardPropsData.ts
@@ -1,4 +1,4 @@
-import { USDTM100ADDRESS_DONAU, USDTMCONTRACTADDRESS_DONAU } from "../../web3/web3";
+import { BTTCTESTNETID, USDTM100ADDRESS_DONAU, USDTMCONTRACTADDRESS_DONAU } from "../../web3/web3";
 import { SelectableCardsParams } from "../SelectableCards";
 
 
@@ -9,9 +9,9 @@ const TRON_A = "Tron";
 const ETH_N = "/imgs/eth_netwrokLogo.png";
 const ETH_A = "ETH";
 
-export function getCardPropsData(cardType: 'Gift Card' | "Cash Note" | "Payment Request"): Array<SelectableCardsParams> {
-
-    if (cardType === "Payment Request") {
+export function getCardPropsData(cardType: 'Gift Card' | "Cash Note" | "Payment Request", netId: string): Array<SelectableCardsParams> {
+    //TODO: ADD NETWORK Number when getting CARD DATA!!!
+    if (cardType === "Payment Request" && netId === BTTCTESTNETID) {
         return [
             { networkAlt: "", networkLogo: "", imageLink: "/imgs/Bunny.svg", imageAlt: "100 USDTM TESTNET", denomination: "100", currency: "USDTM", cardType, erc20Address: USDTMCONTRACTADDRESS_DONAU, noteContractAddress: USDTM100ADDRESS_DONAU },
             // { networkAlt: TRON_A, networkLogo: TRON_N, imageLink: "/imgs/tetherLogo.svg", imageAlt: `100 USDT ${cardType}`, denomination: "100", currency: "USDT", cardType, erc20Address: "", noteContractAddress: "" },
@@ -20,7 +20,7 @@ export function getCardPropsData(cardType: 'Gift Card' | "Cash Note" | "Payment 
             // { networkAlt: ETH_A, networkLogo: ETH_N, imageLink: "/imgs/ethLogo.svg", imageAlt: "1 ETH", denomination: "1", currency: "ETH", cardType, erc20Address: "", noteContractAddress: "" },
             // { networkAlt: BSC_A, networkLogo: BSC_N, imageLink: "/imgs/btcLogo.svg", imageAlt: "0.1 BTC", denomination: "0.1", currency: "BTC", cardType, erc20Address: "", noteContractAddress: "" }
         ]
-    } else {
+    } else if (cardType !== "Payment Request" && netId === BTTCTESTNETID) {
         return [
             { networkAlt: "", networkLogo: "", imageLink: "/imgs/Bunny.svg", imageAlt: "100 USDTM TESTNET", denomination: "100", currency: "USDTM", cardType, erc20Address: USDTMCONTRACTADDRESS_DONAU, noteContractAddress: USDTM100ADDRESS_DONAU },
             // { networkAlt: TRON_A, networkLogo: TRON_N, imageLink: "/imgs/tetherLogo.svg", imageAlt: `10 USDT ${cardType}`, denomination: "10", currency: "USDT", cardType, erc20Address: "", noteContractAddress: "" },
@@ -32,5 +32,7 @@ export function getCardPropsData(cardType: 'Gift Card' | "Cash Note" | "Payment 
             // { networkAlt: BSC_A, networkLogo: BSC_N, imageLink: "/imgs/btcLogo.svg", imageAlt: "0.1 BTC", denomination: "0.1", currency: "BTC", cardType, erc20Address: "", noteContractAddress: "" }
 
         ]
+    } else {
+        return []
     }
 }
