10bc8233332198e9cdb1deb777b6c9bca925d752
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-02-23 05:45:03 +0000

    removed files

--- copyToDist.sh
+++ copyToDist.sh
@@ -2,9 +2,6 @@ cp circuits/withdraw_bunnyNote/withdraw_0001.zkey dist/withdraw_0001.zkey
 
 cp circuits/withdraw_bunnyNote/withdraw_js/withdraw.wasm dist/withdraw.wasm
 
-cp circuits/wallet_owner/isOwner_0001.zkey dist/isOwner_0001.zkey
-cp circuits/wallet_owner/isOwner_js/isOwner.wasm dist/isOwner.wasm
-
 cp abis/ERC20Notes.json dist/ERC20Notes.json
 cp abis/ETHNotes.json dist/ETHNotes.json
 cp abis/BunnyWallet.json dist/BunnyWallet.json
