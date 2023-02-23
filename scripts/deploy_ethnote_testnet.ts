67642702f651d30054a11958d0ff7394e5b289f9
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-02-23 05:46:51 +0000

    deployed on fantom Testnet

--- scripts/deploy_ethnote_testnet.ts
+++ scripts/deploy_ethnote_testnet.ts
@@ -1,6 +1,6 @@
 import { ethers } from "hardhat";
 
-const VERIFIERADDRESS = "0xe5f45dAA32B40742E625a3Ebd1cDf6f343614AdE";
+const VERIFIERADDRESS = "0xD98F9B6AB4fbA9b7173Cfe92Ae1Eb9A3F3F91751"; // Using Fantom Testent address
 const DENOMINATION = ethers.utils.parseEther("1");
 const FEEDIVIDER = 100;
 const RELAYERADDRESS = "0xaaCb9bf503Dfb3A8a77BB5c459f45f495B7ad392"; //New external relayer address
@@ -14,7 +14,9 @@ async function main() {
 
 
   console.log("Bunny Notes ETH Note has been deployed to", ETHNotes.address);
-  // Bunny Notes ETH Note has been deployed to 0x2D524Ee2669b7F521B9d903A56002ba565cc50ba
+  // Bunny Notes ETH Note has been deployed to 0x2D524Ee2669b7F521B9d903A56002ba565cc50ba on Donau Testnet
+  // Bunny Notes ETH Note has been deployed to 0xeE55e7A619343B2f045bfD9A720BF912e1FCfEC7 on Fantom Testnet
+
 }
 
 
--- scripts/deploy_withdrawVerifier_testnet.ts
+++ scripts/deploy_withdrawVerifier_testnet.ts
@@ -9,11 +9,12 @@ async function main() {
     const verifier = await VerifierDeploy.deployed();
 
     console.log('Verifier is deployed to :', verifier.address);
-    // Verifier is deployed to : 0xe5f45dAA32B40742E625a3Ebd1cDf6f343614AdE
+    // Verifier is deployed to : 0xe5f45dAA32B40742E625a3Ebd1cDf6f343614AdE on BTTC Testnet
+    // Verifier is deployed to : 0xD98F9B6AB4fbA9b7173Cfe92Ae1Eb9A3F3F91751 on Fantom Testnet
 }
 
 
-// main().catch(err => {
-//     console.error(err);
-//     process.exitCode = 1;
-// })
+main().catch(err => {
+    console.error(err);
+    process.exitCode = 1;
+})
