a65f1d0b711f150813f1a2a7687592ec10b9283e
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-03-03 20:42:10 +0000

    commitmentQRCode added

--- frontend/qrcode/create.ts
+++ frontend/qrcode/create.ts
@@ -8,3 +8,30 @@ export async function createQR(noteString: string): Promise<any> {
 
     return buffer;
 }
+
+export function getCommitmentQRString(amount: string, currency: string, commitment: string, nullifierHash: string) {
+    return `commitment-${currency}-${amount}-${commitment}-${nullifierHash}`;
+}
+
+export async function commitmentQR({ amount, currency, commitment, nullifierHash }: { amount: string, currency: string, commitment: string, nullifierHash: string }): Promise<any> {
+
+    const commitmentQRString = getCommitmentQRString(amount, currency, commitment, nullifierHash);
+
+    const buffer = await new AwesomeQR({
+        text: commitmentQRString,
+        size: 500
+    }).draw();
+
+    return buffer;
+}
+
+export function commitmentQRStringParser(commitmentString: string) {
+    const commitmentQRCodeRegex = /commitment-(?<currency>\w+)-(?<amount>[\d.]+)-0x(?<commitment>[0-9a-fA-F]{64})-0x(?<nullifierHash>[0-9a-fA-F]{64})/g;
+    const match = commitmentQRCodeRegex.exec(commitmentString);
+    if (!match) {
+        throw new Error("Invalid Commitment QR Code");
+    }
+
+    //@ts-ignore
+    return { currency: match.groups.currency, amount: match.groups.amount, commitment: "0x" + match.groups.commitment, nullifierHash: "0x" + match.groups.nullifierHash }
+}
