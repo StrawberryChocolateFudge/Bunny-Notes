feb680eac217a5613adf2127b0857dddca9ad0a1
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-04-13 18:55:02 +0200

    updated pdf note UI

--- frontend/pdf/index.ts
+++ frontend/pdf/index.ts
@@ -3,15 +3,15 @@ import { CardType } from "../components/CardGrid";
 
 export async function downloadPDF(bearerText: string, denomination: string, commitment: string, cardType: CardType, dataUrl, noteString: string, commitmentQRCodeDataUrl) {
     var doc = new jsPDF("l", "px", "credit-card");
-    doc.setFontSize(7)
+    doc.setFontSize(6)
     doc.text(bearerText, 3, 6);
     doc.addImage(dataUrl, "JPEG", 1, 10, 90, 90);
     doc.setFontSize(12)
     doc.text(cardType, 101, 21);
-    doc.setFontSize(20)
-    doc.text(denomination, 100, 40);
+    doc.setFontSize(10)
+    doc.text(denomination, 101, 40);
     doc.setFontSize(10);
-    doc.text("bunnynotes.finance", 102, 60)
+    doc.text("bunnynotes.finance", 101, 60)
     doc.setFontSize(2)
     doc.text(noteString, 3, 110);
     doc.addPage();
@@ -23,6 +23,7 @@ export async function downloadPDF(bearerText: string, denomination: string, comm
     doc.save(`BunnyNote-${denomination}-${commitment}.pdf`)
     // For testing I opened it in another window!
     // var string = doc.output('datauristring');
+    // console.log(string);
     // var embed = "<embed width='100%' height='100%' src='" + string + "'/>"
     // var x = window.open() as Window;
     // x.document.open();
