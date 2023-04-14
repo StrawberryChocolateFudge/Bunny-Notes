8fea57cb98d730052c5f3f35cf1fc4b8b8e63b19
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-04-14 21:09:56 +0200

    updated bunnyNotes tests

--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,7 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
-import { ERC20Notes, ETHNotes, MOCKERC20 } from "../typechain";
+import { BunnyNotes, MOCKERC20 } from "../typechain";
 
 import { expect } from "chai";
 
@@ -30,9 +30,7 @@ export async function expectRevert(callback: CallableFunction, errorMessage: str
 
 }
 
-export async function setUpNotes(): Promise<{ owner: SignerWithAddress, alice: SignerWithAddress, bob: SignerWithAddress, USDTM: MOCKERC20, Verifier: Contract, ERC20Notes: ERC20Notes, relayer: SignerWithAddress, ETHNotes: ETHNotes, provider: any, attacker: SignerWithAddress }> {
-
-
+export async function setUpBunnyNotes(): Promise<{ owner: SignerWithAddress, alice: SignerWithAddress, bob: SignerWithAddress, USDTM: MOCKERC20, Verifier: Contract, relayer: SignerWithAddress, bunnyNotes: BunnyNotes, provider: any, attacker: SignerWithAddress }> {
     const [owner, alice, bob, attacker, relayer] = await ethers.getSigners();
 
     const MockERC20Factory = await ethers.getContractFactory("MOCKERC20");
@@ -47,14 +45,11 @@ export async function setUpNotes(): Promise<{ owner: SignerWithAddress, alice: S
     const VerifierDeploy = await VerifierFactory.deploy();
     const Verifier = await VerifierDeploy.deployed();
 
-    const ERC20NotesFactory = await ethers.getContractFactory("ERC20Notes");
-    const ETHNotesFactory = await ethers.getContractFactory("ETHNotes");
-
-
-    const ERC20Notes: ERC20Notes = await ERC20NotesFactory.deploy(VerifierDeploy.address, DENOMINATION, FEEDIVIDER, MockERC20.address, relayer.address);
-    const ETHNotes: ETHNotes = await ETHNotesFactory.deploy(VerifierDeploy.address, DENOMINATION, FEEDIVIDER, relayer.address);
+    const BunnyNotesFactory = await ethers.getContractFactory("BunnyNotes");
+    const bunnyNotesDeploy = await BunnyNotesFactory.deploy(Verifier.address);
+    const bunnyNotes = await bunnyNotesDeploy.deployed();
 
     const provider = ethers.provider;
 
-    return { owner, alice, bob, attacker, USDTM, Verifier, ERC20Notes, relayer, ETHNotes, provider }
+    return { owner, alice, bob, attacker, USDTM, Verifier, bunnyNotes, relayer, provider }
 }
