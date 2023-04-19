7a87702439de9bfc0c0b0d0b9772da4933c1a305
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-04-19 20:50:45 +0200

    terms and conditons

---
+++ frontend/pdf/termsAndConditions.tsx
@@ -0,0 +1,15 @@
+export const TermsAndConditionsTitle = `Terms and Conditions for Bunny Note`
+
+export const TermsAndConditionsSubtitle = `The following terms and conditions apply to the use of Bunny Notes on the website bunnynotes.finance:`
+
+export const Purpose = `Purpose: A bunny note is a printable and verifiable claim to withdraw value that was deposited into a smart contract through the website bunnynotes.finance. It is designed to facilitate the transfer of value between parties who deposit into and withdraw funds from a smart contract.`
+
+export const Indemnification = `Indemnification: The creator of the smart contract and the website bunnynotes.finance are indemnified from any claims, damages, or losses arising from the use of Bunny Note. Parties acknowledge and agree that they use Bunny Note at their own risk and that the creator of the smart contract and the website bunnynotes.finance shall not be liable for any damages, losses, or expenses arising from the use of Bunny Note.`
+
+export const Verification = `Verification: Bunny Note can be verified on the blockchain. Parties acknowledge and agree that they are solely responsible for verifying the authenticity of the Bunny Note and ensuring that it has not been tampered with.`
+
+export const Transferability = `Transferability: Bunny Note is transferable and can be redeemed by the holder of the note. Parties acknowledge and agree that they are solely responsible for transferring the note to the intended recipient through the website bunnynotes.finance and that the creator of the smart contract and the website bunnynotes.finance shall not be liable for any losses or damages arising from the transfer of the note.`
+
+export const GoverningLaw = `Governing Law and Jurisdiction: These terms and conditions shall be governed by and construed in accordance with the laws of the jurisdiction where the user is located. Any dispute arising from these terms and conditions shall be resolved in the courts of the jurisdiction where the user is located.`
+
+export const endTerms = `By using Bunny Note on the website bunnynotes.finance, parties acknowledge and agree to be bound by these terms and conditions. If parties do not agree to these terms and conditions, they should not use the Bunny Note and the website bunnynotes.finance.`;
