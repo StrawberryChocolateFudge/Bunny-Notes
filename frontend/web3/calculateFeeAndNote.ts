c20c6de836c8bfae88ba5abb4f28c8c6739a7a0d
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-04-19 20:51:08 +0200

    removed parseFloat

--- frontend/web3/calculateFeeAndNote.ts
+++ frontend/web3/calculateFeeAndNote.ts
@@ -3,7 +3,7 @@ import { calculateFeeLocally } from "./web3";
 
 export async function calculateFeeAndNote(denomination: string, currency: string, netId: string) {
     const fee = calculateFeeLocally(denomination);
-    const noteDetails = await createNote(currency, parseFloat(denomination), netId);
+    const noteDetails = await createNote(currency, denomination, netId);
 
     return { noteDetails, fee }
 }
