eaee249f89e1000c3b485ce0156ec3a31d0fe492
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-04-22 05:52:24 +0200

    updated token card rendering

--- frontend/components/CardGrid.tsx
+++ frontend/components/CardGrid.tsx
@@ -7,24 +7,38 @@ export type CardType = "Bunny Note" | "Spending Note" | "Payment Request";
 
 interface CardGridProps {
     cardProps: Array<SelectableCardsParams>
-    handleSelect: (currency: string, cardType: CardType, tokenAddress: string, isCustom: boolean) => void
+    handleSelect: (currency: string, cardType: CardType, tokenAddress: string, isCustom: boolean, isFeeless: boolean) => void
 
 }
 
 
 export default function CardGrid(props: CardGridProps) {
+    const [windowSize, setWindowSize] = React.useState(window.innerWidth);
+    const handleWindowResize = React.useCallback(event => {
+        setWindowSize(window.innerWidth);
+    }, []);
+
+    React.useEffect(() => {
+        window.addEventListener('resize', handleWindowResize);
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, [handleWindowResize]);
+
+    const useMarginAuto = windowSize < 457 ? "0 auto" : "";
+
 
     const renderGrid = () => {
         return props.cardProps.map(data => {
-            return <Grid item xs={6} key={`${data.currency}${data.cardType}`} justifyContent="center" alignItems="stretch" sx={{ textAlign: "center", maxWidth: "100%" }}>
-                <SelectableCards isCustom={data.isCustom} erc20Address={data.erc20Address} networkAlt={data.networkAlt} networkLogo={data.networkLogo} handleSelect={props.handleSelect} cardType={data.cardType} currency={data.currency} imageAlt={data.imageAlt} imageLink={data.imageLink}></SelectableCards>
+            return <Grid item xs={4} key={`${data.currency}${data.cardType}`} justifyContent="center" alignItems="stretch" sx={{ textAlign: "center", maxWidth: "100%", margin: useMarginAuto }}>
+                <SelectableCards handleSelect={props.handleSelect} {...data}></SelectableCards>
             </Grid>
         })
     }
 
     return (
         <Box sx={{ marginTop: "20px", marginLeft: "20px", marginRight: "20px", marginBottom: "40px" }}>
-            <Grid justifyContent={"center"} container rowSpacing={5} sx={{ margin: "0 auto" }} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid justifyContent={"flex-start"} flexWrap="wrap" container spacing={3} >
                 {renderGrid()}
             </Grid>
         </Box>
