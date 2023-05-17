944187b6da710d9311b99fd8e68c0b73f222285c
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-05-17 05:32:28 +0200

    FEAT: added proof generation and verification

--- lib/generateProof.ts
+++ lib/generateProof.ts
@@ -2,6 +2,7 @@ import { Deposit, SnarkArtifacts } from "./types";
 //@ts-ignore
 import { groth16 } from "snarkjs"
 import { FullProof } from "./types"
+import { EncodedForCircuit } from "./merkleTree";
 
 
 // Generates the proofs for verification! 
@@ -51,4 +52,58 @@ export function verifyThreePublicSignals(verificationKey: any, { proof, publicSi
         ],
         proof
     )
+}
+
+export function verifyFourPublicSignals(verificationKey: any, { proof, publicSignals }: FullProof): Promise<boolean> {
+    return groth16.verify(
+        verificationKey,
+        [
+            publicSignals[0],
+            publicSignals[1],
+            publicSignals[2],
+            publicSignals[3],
+        ],
+        proof
+    )
+}
+
+export async function generateBundleWithdrawProof(
+    {
+        deposit,
+        root,
+        merkleProof,
+        recipient,
+        snarkArtifacts
+    }: {
+        deposit: Deposit,
+        root: BigInt,
+        merkleProof: EncodedForCircuit,
+        recipient: string,
+        snarkArtifacts?: SnarkArtifacts
+    }): Promise<FullProof> {
+    console.log("Generate proof start");
+    const input = {
+        nullifierHash: deposit.nullifierHash,
+        commitmentHash: deposit.commitment,
+        recipient,
+        root,
+
+        // Private snark inputs
+        nullifier: deposit.nullifier,
+        secret: deposit.secret,
+        pathElements: merkleProof.pathElements,
+        pathIndices: merkleProof.pathIndices
+    }
+
+    if (!snarkArtifacts) {
+        snarkArtifacts = {
+            wasmFilePath: "circuits/withdraw_bunnyBundle/withdrawBundledNote_js/withdrawBundledNote.wasm",
+            zkeyFilePath: "circuits/withdraw_bunnyBundle/withdrawBundledNote_0001.zkey"
+        }
+    }
+
+    console.time("Proof time");
+    const { proof, publicSignals } = await groth16.fullProve(input, snarkArtifacts.wasmFilePath, snarkArtifacts.zkeyFilePath)
+    console.timeEnd("Proof time");
+    return { proof, publicSignals }
 }
