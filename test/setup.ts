fd1d7e6ec7fbad65f85f82c7e800b3545c34641c
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-05-17 06:20:21 +0200

    created setup for bunnyBundles contracts

--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,7 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
-import { BunnyNotes, ERC20, MOCKERC20, TokenSale } from "../typechain";
+import { BunnyBundles, BunnyNotes, ERC20, MOCKERC20, TokenSale } from "../typechain";
 
 import { expect } from "chai";
 
@@ -61,4 +61,36 @@ export async function setUpBunnyNotes(): Promise<{ owner: SignerWithAddress, ali
     const provider = ethers.provider;
 
     return { owner, alice, bob, attacker, USDTM, Verifier, bunnyNotes, relayer, provider, feelesstoken, tokensale }
+}
+
+export async function setUpBunnyBundles(): Promise<SetUpBundlesType> {
+    const [owner, alice, bob, attacker] = await ethers.getSigners();
+
+    const MockERC20Factory = await ethers.getContractFactory("MOCKERC20");
+    const MockERC20 = await MockERC20Factory.deploy();
+    const USDTM = await MockERC20.deployed();
+
+
+    const FeeLessTokenFactory = await ethers.getContractFactory("MOCKERC20");
+    const feelessTokenDeploy = await FeeLessTokenFactory.deploy();
+    const feelesstoken = await feelessTokenDeploy.deployed();
+
+    const VerifierFactory = await ethers.getContractFactory("contracts/WithdrawBundledNotes.sol:Verifier");
+    const VerifierDeploy = await VerifierFactory.deploy();
+    const Verifier = await VerifierDeploy.deployed();
+
+    const BunnyBundlesFactory = await ethers.getContractFactory("BunnyBundles");
+    const bunnyBundlesDeploy = await BunnyBundlesFactory.deploy(Verifier.address, feelesstoken.address);
+    const bunnyBundles = await bunnyBundlesDeploy.deployed();
+
+    return { owner, alice, bob, attacker, bunnyBundles, USDTM, feelesstoken };
+}
+type SetUpBundlesType = {
+    owner: SignerWithAddress,
+    alice: SignerWithAddress,
+    bob: SignerWithAddress,
+    attacker: SignerWithAddress,
+    bunnyBundles: BunnyBundles,
+    USDTM: MOCKERC20,
+    feelesstoken: MOCKERC20
 }
