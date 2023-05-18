a7f91f60092ee5a5b79b4920825c38e475c7dbd5
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-05-18 03:01:55 +0200

    Updated chip style

--- frontend/components/utils/CheckWebsiteURLWarning.tsx
+++ frontend/components/utils/CheckWebsiteURLWarning.tsx
@@ -4,7 +4,13 @@ import React from "react";
 interface CheckWebsiteURLWarningProps { }
 
 export function CheckWebsiteURLWarning(props: CheckWebsiteURLWarningProps) {
-    //@ts-ignore
-    return <Chip sx={{ borderRadius: "0" }} color="warning" label={<marquee>Beware of Phishing! Make sure the url is https://bunnynotes.finance !</marquee>} />
 
+    return <Chip sx={{
+        borderRadius: "0", '& .MuiChip-label': {
+            width: "100%"
+        }
+
+    }} color="primary"
+        //@ts-ignore
+        label={<marquee>Beware of Phishing! Make sure the url is https://bunnynotes.finance !</marquee>} />
 }
