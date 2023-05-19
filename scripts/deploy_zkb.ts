d78082a6f327b60b2552af7861d4943bbcfecb14
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-05-19 10:51:11 +0200

    updated zkb token deploy script

--- scripts/deploy_zkb.ts
+++ scripts/deploy_zkb.ts
@@ -7,7 +7,6 @@ async function main() {
     const ZKBDeploy = await ZKBFactory.deploy(initialSupply);
     const zkbtoken = await ZKBDeploy.deployed();
     console.log("ZKB Token deployed to ", zkbtoken.address);
-    // ZKB Token deployed to  0x72b5bD690A3253e4B6cCB7A4bAe53EC34b1e8Df2 on Donau testnet
     // ZKB Token deployed to  0xeDc320436A3d390B65Dfc0dc868909c914F431cA on BSC testnet
 }
 
