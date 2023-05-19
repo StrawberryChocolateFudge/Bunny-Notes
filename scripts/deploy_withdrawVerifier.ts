b7f9fa604a35fb8efff24dd6845ffcfaea8c79c5
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-05-19 10:50:41 +0200

    updated deployed verifiers

--- scripts/deploy_withdrawVerifier.ts
+++ scripts/deploy_withdrawVerifier.ts
@@ -1,6 +1,5 @@
 import { ethers } from "hardhat";
 
-// /?TODO: DEPLOY ON BSC MAINNET!!
 async function main() {
 
 
@@ -10,7 +9,7 @@ async function main() {
 
     console.log('Verifier is deployed to :', verifier.address);
     // Verifier is deployed to : 0x02B721A1dB666CE9E6dF903BE2664872D9F98345 on Donau testnet // Latest
-    // Verifier is deployed to : 0x18a8C98DBfe92d739f4134493F39cE8b692f323B on BSC Testnet!! 
+    // Verifier is deployed to : 0x18a8C98DBfe92d739f4134493F39cE8b692f323B on BSC Testnet!! // Latest yeah
     // Verifier is deployed to : 0x5586938a2fC4489661E868c5800769Fb10847fC5 on BTT MAINNET!!// latest
 }
 
