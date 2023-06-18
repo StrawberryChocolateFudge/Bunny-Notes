ed17e9ca84e0a6e9b5736513d963a07b1a6549ca
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-06-18 22:33:05 +0200

    added bunny bundle create and get fee

--- frontend/web3/calculateFeeAndNote.ts
+++ frontend/web3/calculateFeeAndNote.ts
@@ -1,9 +1,24 @@
 import { createNote } from "../zkp/generateProof";
 import { calculateFeeLocally } from "./web3";
+import { createBundle } from "../../lib/BunnyBundle";
+export async function calculateFeeAndNote(
+  denomination: string,
+  currency: string,
+  netId: string,
+) {
+  const fee = calculateFeeLocally(denomination);
+  const noteDetails = await createNote(currency, denomination, netId);
 
-export async function calculateFeeAndNote(denomination: string, currency: string, netId: string) {
-    const fee = calculateFeeLocally(denomination);
-    const noteDetails = await createNote(currency, denomination, netId);
+  return { noteDetails, fee };
+}
 
-    return { noteDetails, fee }
+export async function calculateFeeAndBundle(
+  totalValue: string,
+  size: number,
+  currency: string,
+  netId: number,
+) {
+  const fee = calculateFeeLocally(totalValue);
+  const bundle = await createBundle({ currency, totalValue, size, netId });
+  return { bundle, fee };
 }
