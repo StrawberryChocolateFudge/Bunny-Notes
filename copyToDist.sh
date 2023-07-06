2593596277c453368a2edbc8298470ef1f020ade
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-07-06 23:00:14 +0200

    added bunny bundle verification

--- copyToDist.sh
+++ copyToDist.sh
@@ -2,6 +2,9 @@ cp circuits/withdraw_bunnyNote/withdraw_final.zkey dist/withdraw_final.zkey
 
 cp circuits/withdraw_bunnyNote/withdraw_js/withdraw.wasm dist/withdraw.wasm
 
+cp circuits/withdraw_bunnyBundle/withdrawBundledNote_final.zkey dist/withdraw_bunnyBundle_final.zkey
+cp circuits/withdraw_bunnyBundle/withdrawBundledNote_js/withdrawBundledNote.wasm dist/withdrawBundledNote.wasm
+
 cp abis/BunnyNotes.json dist/BunnyNotes.json
 cp abis/BunnyBundles.json dist/BunnyBundles.json
 
